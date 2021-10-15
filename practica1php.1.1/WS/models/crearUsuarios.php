ddd5090d3bf714f53fbadc525c36adb4d2b6d580
Ekaterina Adarchenko <90846227+eadarchenko@users.noreply.github.com>
2021-10-15 22:40:57 +0200

    Add files via upload

---
+++ practica1php.1.1/WS/models/crearUsuarios.php
@@ -0,0 +1,33 @@
+<?php
+
+if(!$_POST) {
+    echo "Rellene el formulario"
+}
+
+else
+{
+    $nombre=$_POST['nombre'];
+    $apellido=$_POST['apellido'];
+    $contrasena=$_POST['contrasena'];
+    $telefono=$_POST['telefono'];
+    $email=$_POST['email'];
+    $sexo=$_POST['sexomf'];
+
+$user=new User($nombre, $apellido, $contrasena, $telefono, $email, $sexo);
+
+
+$file_nombre="datos.txt";
+$file_modo="a+";
+
+
+$file=fopen($file_nombre, $file_modo);
+
+fwrite($file, $user->toString());
+
+fclose($file);
+
+
+
+echo json_decode($user);
+
+}
